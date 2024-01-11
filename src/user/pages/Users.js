df0103b09c2a5541847e089fd75624db75da486b
agpavlik <agpavlik@gmail.com>
2024-01-11 18:56:28 -0500

    Add UserItem.js

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users !</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      image: "image",
+      name: "Alex Grant",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
