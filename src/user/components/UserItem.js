ffed0934395ded2e55f4f261a58fa43f867e4863
agpavlik <agpavlik@gmail.com>
2024-01-11 20:49:36 -0500

    Add Avatar, Card and styles

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,20 +1,27 @@
 import React from "react";
+
+import { Link } from "react-router-dom";
+import Avatar from "../../shared/components/UAElements/Avatar";
+import Card from "../../shared/components/UAElements/Card";
+
 import "./UserItem.css";
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,19 +5,22 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      image: "image",
+      image:
+        "https://images.pexels.com/photos/1452128/pexels-photo-1452128.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load",
       name: "Alex Grant",
       places: 3,
     },
     {
       id: "u2",
-      image: "image",
+      image:
+        "https://images.pexels.com/photos/1850732/pexels-photo-1850732.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load",
       name: "Max Teo",
       places: 3,
     },
     {
-      id: "u2",
-      image: "image",
+      id: "u3",
+      image:
+        "https://images.pexels.com/photos/1918052/pexels-photo-1918052.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load",
       name: "Rico Nord",
       places: 3,
     },
