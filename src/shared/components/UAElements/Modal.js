2620a3e3adee7a73dd64b5e4d6dc8e88feb5b433
agpavlik <agpavlik@gmail.com>
2024-01-13 22:25:42 -0500

    Add modal window for places

---
+++ src/shared/components/UAElements/Modal.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+
+import Backdrop from "./Backdrop";
+import "./Modal.css";
+
+// Extra flexibility for the className -> there are in template literal not only modal class
+// but also prop.className so that allows add new classes to the modal.
+
+// Extra flexibility for the form -> add an onSubmit handler and check if props onSubmit is set and call it.
+// Otherwise pass an inline function where get the event and event prevent default to make sure that if
+// we render any buttons inside of the form, we don't accidentally reload the page by triggering form submission.
+
+const ModalOverlay = (props) => {
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
+};
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </React.Fragment>
+  );
+};
+
+export default Modal;
