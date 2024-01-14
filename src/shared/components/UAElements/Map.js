92ac05d1ff970dce254707ae15b8113f81fdc9cd
agpavlik <agpavlik@gmail.com>
2024-01-14 01:16:13 -0500

    Fix errors with Map

--- src/shared/components/UAElements/Map.js
+++ src/shared/components/UAElements/Map.js
@@ -7,13 +7,30 @@ const Map = (props) => {
 
   const { center, zoom } = props;
 
-  useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom,
-    });
+  // Google map
+  // useEffect(() => {
+  //   const map = new window.google.maps.Map(mapRef.current, {
+  //     center: center,
+  //     zoom: zoom,
+  //   });
 
-    new window.google.maps.Marker({ position: center, map: map });
+  //   new window.google.maps.Marker({ position: center, map: map });
+  // }, [center, zoom]);
+
+  // Alternative - OpenLayers
+  useEffect(() => {
+    new window.ol.Map({
+      target: mapRef.current.id,
+      layers: [
+        new window.ol.layer.Tile({
+          source: new window.ol.source.OSM(),
+        }),
+      ],
+      view: new window.ol.View({
+        center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+        zoom: zoom,
+      }),
+    });
   }, [center, zoom]);
 
   return (
@@ -21,6 +38,7 @@ const Map = (props) => {
       ref={mapRef}
       className={`map ${props.className}`}
       style={props.style}
+      id="map"
     ></div>
   );
 };
