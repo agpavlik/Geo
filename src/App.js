c5b3cd074b994e68546bbea518030b9220f28345
agpavlik <agpavlik@gmail.com>
2024-02-15 19:38:10 -0500

    Add Login form

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 const App = () => {
   return (
@@ -39,6 +40,9 @@ const App = () => {
           </Route>
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
