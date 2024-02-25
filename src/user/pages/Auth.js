ef902a4aafc6b53d9f8b3497beb171d3c61ea095
agpavlik <agpavlik@gmail.com>
2024-02-24 23:18:25 -0500

    Complete file upload

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -89,17 +89,16 @@ const Auth = () => {
     } else {
       // signup
       try {
+        // Use formData instead of JSON which can not read image data.
+        const formData = new FormData();
+        formData.append("email", formState.inputs.email.value);
+        formData.append("name", formState.inputs.name.value);
+        formData.append("password", formState.inputs.password.value);
+        formData.append("image", formState.inputs.image.value);
         const responseData = await sendRequest(
           "http://localhost:5000/api/users/signup",
           "POST",
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          {
-            "Content-Type": "application/json",
-          }
+          formData
         );
 
         auth.login(responseData.user.id);
@@ -127,7 +126,12 @@ const Auth = () => {
             />
           )}
           {!isLoginMode && (
-            <ImageUpload center id="image" onInput={inputHandler} />
+            <ImageUpload
+              center
+              id="image"
+              onInput={inputHandler}
+              errorText="Please provide an image."
+            />
           )}
           <Input
             element="input"
