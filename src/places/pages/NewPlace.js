0d36fd01910d64cb89f41c41fbb03a43eea11c9f
agpavlik <agpavlik@gmail.com>
2024-03-01 20:51:37 -0500

    Preparation for build

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -53,9 +53,14 @@ const NewPlace = () => {
       formData.append("description", formState.inputs.description.value);
       formData.append("address", formState.inputs.address.value);
       formData.append("image", formState.inputs.image.value);
-      await sendRequest("http://localhost:5000/api/places", "POST", formData, {
-        Authorization: "Bearer " + auth.token,
-      });
+      await sendRequest(
+        process.env.REACT_APP_BACKEND_URL + "/places",
+        "POST",
+        formData,
+        {
+          Authorization: "Bearer " + auth.token,
+        }
+      );
       history.push("/");
     } catch (err) {}
   };
