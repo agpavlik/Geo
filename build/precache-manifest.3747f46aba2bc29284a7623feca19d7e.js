872e95096a983e271e4e837ccbd779824fc498ac
agpavlik <agpavlik@gmail.com>
2024-03-02 23:59:17 -0500

    Preparation for deploying

---
+++ build/precache-manifest.3747f46aba2bc29284a7623feca19d7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639062d1788f1a15a275beef00e87163",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6ffc548f3d651e2e5b",
+    "url": "/static/css/main.84833006.chunk.css"
+  },
+  {
+    "revision": "fadf4975768dd9b0fd4d",
+    "url": "/static/js/2.546a1c6a.chunk.js"
+  },
+  {
+    "revision": "0c6ffc548f3d651e2e5b",
+    "url": "/static/js/main.18bcf073.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  }
+]);
